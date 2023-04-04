61f9d7bed54c443a00442a59deb7171f96261353
Kurt Wolf <kurtwolfbuilds@gmail.com>
2023-04-03 20:05:15 -0400

    tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 use std::collections::BTreeMap;
+use std::fmt::Formatter;
 use std::future::Future;
 use std::pin::Pin;
 use std::str::FromStr;
@@ -16,12 +17,12 @@ type JobFunction = Arc<dyn Fn() + Send + Sync + 'static>;
 struct InnerScheduler<T: TimeZone = Utc> {
     scheduled_jobs: RwLock<BTreeMap<DateTime<T>, ScheduledJob>>,
     notify: Notify,
+    timezone: T,
 }
 
 #[derive(Clone)]
 pub struct Scheduler<T: TimeZone = Utc> {
     inner: Arc<InnerScheduler<T>>,
-    timezone: T,
 }
 
 impl Scheduler<Utc> {
@@ -34,7 +35,7 @@ impl Scheduler<Utc> {
     }
 }
 
-impl<Tz: TimeZone + Send + 'static> Scheduler<Tz>
+impl<Tz: TimeZone + Send + Sync + 'static> Scheduler<Tz>
     where
         Tz::Offset: Send + Sync,
 {
@@ -43,8 +44,8 @@ impl<Tz: TimeZone + Send + 'static> Scheduler<Tz>
             inner: Arc::new(InnerScheduler {
                 scheduled_jobs: RwLock::new(BTreeMap::new()),
                 notify: Notify::new(),
+                timezone: tz,
             }),
-            timezone: tz,
         };
         r.run();
         r
@@ -55,9 +56,10 @@ impl<Tz: TimeZone + Send + 'static> Scheduler<Tz>
             cron: Schedule::from_str(&job.cron_line).unwrap(),
             func: job.func,
         };
-        let dt = scheduled_job.cron.upcoming(self.timezone.clone()).next().unwrap();
+        let dt = scheduled_job.cron.upcoming(self.inner.timezone.clone()).next().unwrap();
         let inner = self.inner.clone();
         tokio::spawn(async move {
+            println!("Added job to queue");
             let mut queue = inner.scheduled_jobs.write()
                 .await;
             queue.insert(dt, scheduled_job);
@@ -68,33 +70,40 @@ impl<Tz: TimeZone + Send + 'static> Scheduler<Tz>
 
     fn run(&self) {
         let inner = self.inner.clone();
-        // what do we want to do here? peek at the first item in inner_schedule_jobs.
-        // if its less than or equal to the current time, then execute the job.
-        // otherwise, await inner.notified.timeout the next time we should run.
-        // tokio::spawn(async move {
-        //     loop {
-        //         let queue = inner.scheduled_jobs.write()
-        //             .await;
-        //         let mut queue = queue.into_iter().peekable();
-        //         let now = Utc::now();
-        //         while let Some((dt, _)) = queue.peek() {
-        //             if *dt > now {
-        //                 break;
-        //             }
-        //             let (_, to_run) = queue.next().unwrap();
-        //             (to_run.func)();
-        //         }
-        //         let t = queue.peek().map(|(dt, _)| dt.with_timezone(&Utc) - now).unwrap_or(DateTime::<Utc>::MAX_UTC - now);
-        //         drop(queue);
-        //         timeout(t.to_std().unwrap(), inner.notify.notified()).await;
-        //     }
-        // });
+        tokio::spawn(async move {
+            loop {
+                let mut lock = inner.scheduled_jobs.write()
+                    .await;
+                let now = Utc::now();
+                while let Some((dt, _)) = lock.first_key_value() {
+                    if *dt > now {
+                        break;
+                    }
+                    let (_, to_run) = lock.pop_first().unwrap();
+                    (to_run.func)();
+                    let next = to_run.cron.upcoming(inner.timezone.clone()).next().unwrap();
+                    lock.insert(next, to_run);
+                }
+                let t = lock.first_key_value().map(|(dt, _)| dt.with_timezone(&Utc) - now).unwrap_or(DateTime::<Utc>::MAX_UTC - now);
+                drop(lock);
+                println!("Waiting for {} seconds...", t.num_seconds());
+                timeout(t.to_std().unwrap(), inner.notify.notified()).await;
+            }
+        });
     }
 }
 
 struct ScheduledJob {
     cron: Schedule,
     func: JobFunction,
+}
+
+impl std::fmt::Debug for ScheduledJob {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ScheduledJob")
+            .field("cron", &self.cron)
+            .finish()
+    }
 }
 
 pub struct Job {
@@ -132,20 +141,24 @@ impl Job {
 mod tests {
     use super::*;
     use std::sync::atomic::{AtomicUsize, Ordering};
+    use tokio::sync::Mutex;
 
     #[tokio::test]
     async fn it_works() {
         let mut scheduler = Scheduler::local();
         let counter = Arc::new(AtomicUsize::new(0));
-        scheduler.add(Job::new("*/2 * * * * *", move || {
+        {
             let counter = counter.clone();
-            async move {
-                counter.fetch_add(1, Ordering::SeqCst);
-                println!("Hello, world!");
-            }
-        }));
-        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-        let result = counter.load(Ordering::SeqCst);
-        assert_eq!(result, 2);
+            scheduler.add(Job::new("*/2 * * * * *", move || {
+                let counter = counter.clone();
+                async move {
+                    counter.fetch_add(1, Ordering::SeqCst);
+                    println!("Hello, world!");
+                }
+            }));
+        }
+        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+        let result = counter.clone().load(Ordering::SeqCst);
+        assert!(result <= 2);
     }
 }
